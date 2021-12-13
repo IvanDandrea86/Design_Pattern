a98c41d9aab9af2015a1915f188c7f53c84b2ff1
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2021-12-13 12:45:31 +0100

    another example of Oberver Patter

---
+++ 02-Observer-Pattern/socialMedia.ts
@@ -0,0 +1,55 @@
+interface ISubject {
+    subscribe(observer: Observer):void
+    unsubscribe(observer: Observer):void
+    notify(news:String):void
+}
+
+interface IObserver {
+    update(news:string):void
+}
+
+class SocialMedia implements ISubject {
+    private observers:Observer[] = [];
+    subscribe(observer:Observer) {
+        this.observers.push(observer)
+    }
+    unsubscribe(observer:Observer) {
+        this.observers = this.observers.filter((element)=>{
+            return observer.name !== element.name
+        })
+    }
+    notify(news:string) {
+        this.observers.forEach(observer => {
+            observer.update(news);
+        })
+    }
+}
+
+class Observer implements IObserver {
+    constructor(public readonly name:string) {}
+    private feed:string[] = []; 
+    update(news:string) {
+        this.feed.push(news)
+       console.log(`${this.name} recieved  a news`)
+    }
+    showFeed() {
+        console.log(this.name + ":" + this.feed)
+    }
+}
+export default ()=>{
+const firstFan = new Observer("Alice");
+const secondFan = new Observer("Bob");
+const thirdFan =new Observer("Ivan")
+const newsFeed = new SocialMedia();
+
+newsFeed.subscribe(firstFan);
+newsFeed.subscribe(secondFan);
+newsFeed.notify("\n-Today is gonna rain");
+newsFeed.unsubscribe(secondFan);
+newsFeed.notify("\n-Go by some food");
+newsFeed.subscribe(thirdFan);
+newsFeed.notify("\n-President say:\"bla bla..\"");
+firstFan.showFeed();
+secondFan.showFeed()
+thirdFan.showFeed();
+}
--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import observer from "./02-Observer-Pattern/observer";
 import strategyPattern from "./01-Strategy-Patterns/strategyPattern";
+import socialMedia from "./02-Observer-Pattern/socialMedia";
 
 
 
 console.log("=============Design-Pattern-Example========\n")
 strategyPattern();
-console.log("=============Observer-Pattern-Example========\n")
+console.log("=============Observer-Pattern-Example 1========\n")
 observer();
+console.log("=============Observer-Pattern-Example 2========\n")
+socialMedia()
